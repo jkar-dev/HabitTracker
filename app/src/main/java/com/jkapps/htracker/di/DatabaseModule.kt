cf10a21068095f2166fafcd91a3a10a4c5ac4e69
Julia Karavaeva <karavaeva.jul@gmail.com>
2021-01-15 11:34:45 +0300

    migrate to hilt 2.31-alpha

--- app/src/main/java/com/jkapps/htracker/di/DatabaseModule.kt
+++ app/src/main/java/com/jkapps/htracker/di/DatabaseModule.kt
@@ -7,11 +7,11 @@ import com.jkapps.htracker.data.local.HabitDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 object DatabaseModule {
 
--- app/src/main/java/com/jkapps/htracker/di/RepositoryModule.kt
+++ app/src/main/java/com/jkapps/htracker/di/RepositoryModule.kt
@@ -6,9 +6,9 @@ import com.jkapps.htracker.domain.HabitRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 object RepositoryModule {
 
