8243a328d693c7e242d4b984b2c280f1bcf0ebfe
Julia Karavaeva <karavaeva.jul@gmail.com>
2021-01-17 10:56:40 +0300

    fix bug with the saved date

--- app/src/main/java/com/jkapps/htracker/data/local/preferences/PreferenceDataStore.kt
+++ app/src/main/java/com/jkapps/htracker/data/local/preferences/PreferenceDataStore.kt
@@ -8,6 +8,7 @@ import androidx.datastore.preferences.edit
 import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import timber.log.Timber
 
 class PreferenceDataStore(
     context: Context
@@ -15,17 +16,19 @@ class PreferenceDataStore(
     private val dataStore: DataStore<Preferences> = context.createDataStore(dataStoreName)
 
     val lastSavedDate : Flow<String> = dataStore.data.map {
+        Timber.i("Last saved date: ${it[PreferenceKeys.LAST_SAVED_DATE]}")
         it[PreferenceKeys.LAST_SAVED_DATE] ?: ""
     }
 
     suspend fun saveDate(date : String) {
         dataStore.edit {
+            Timber.i("Save date: $date")
             it[PreferenceKeys.LAST_SAVED_DATE] = date
         }
     }
 
     private object PreferenceKeys {
-        val LAST_SAVED_DATE = preferencesKey<String>("last_saved_day")
+        val LAST_SAVED_DATE = preferencesKey<String>("last_saved_date")
     }
 
     companion object {
--- app/src/main/java/com/jkapps/utils/DateHelper.kt
+++ app/src/main/java/com/jkapps/utils/DateHelper.kt
@@ -1,9 +1,9 @@
 package com.jkapps.utils
 
+import java.text.DateFormat
 import java.util.*
 
 object DateHelper {
-    private const val dateFormat = "dd-MM-yy"
-
+    private val dateFormat = DateFormat.getDateInstance(DateFormat.SHORT)
     val currentDay : String = dateFormat.format(Date())
 }
